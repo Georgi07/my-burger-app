aee22e61e3b6a4ad49140b83613219c9b8a36df7
unknown <g.nikolaev@abv.bg>
2020-08-16 14:30:26 +0300

    Updates

---
+++ precache-manifest.4b2c3c2c5e3f2eb5c74063919d161feb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e08eb01022bf84cd51e0148e66a1c57a",
+    "url": "/burger-builder-app/index.html"
+  },
+  {
+    "revision": "bbea977f3874c32759b9",
+    "url": "/burger-builder-app/static/css/main.2f9632ed.chunk.css"
+  },
+  {
+    "revision": "0549bc0c7dd7c261518f",
+    "url": "/burger-builder-app/static/js/2.80c5e3a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/burger-builder-app/static/js/2.80c5e3a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbea977f3874c32759b9",
+    "url": "/burger-builder-app/static/js/main.cb2e2280.chunk.js"
+  },
+  {
+    "revision": "ae1e34274cec123a6b79",
+    "url": "/burger-builder-app/static/js/runtime-main.d2df3248.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder-app/static/media/burger-logo.b8503d26.png"
+  }
+]);
